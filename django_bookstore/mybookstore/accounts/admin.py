db147c591d335c8e20db5c7bb81e78af958437c2
AnanyaInala <inalaananya@gmail.com>
2025-08-01 09:56:24 +0530

    Delete django_bookstore/mybookstore directory

--- django_bookstore/mybookstore/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-# accounts/admin.py
-
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',)
-    search_fields = ('user__username',)
--- django_bookstore/mybookstore/orders/urls.py
+++
@@ -1,8 +0,0 @@
-# orders/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.order_history, name='order_history'), # Displays user's order history
-]
